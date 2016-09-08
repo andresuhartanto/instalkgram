40d6ecd8814d1a39f35d335942af9c0e110950ef
andresuhartanto <andresuhartanto28@gmail.com>
2016-09-08 11:32:10 +0800

    Initial Commit

---
+++ instalkgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instalkgram
+//
+//  Created by Andre Suhartanto on 9/8/16.
+//  Copyright © 2016 EndeJeje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
