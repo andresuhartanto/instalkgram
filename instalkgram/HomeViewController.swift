cb71f41776ec64d026625450dcbcca80f35e809a
ggezzz <ggezzz@yahoo.com>
2016-09-13 10:59:12 +0800

    doing relation

--- instalkgram/HomeViewController.swift
+++ instalkgram/HomeViewController.swift
@@ -16,7 +16,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var feedTableView: UITableView!
     var imagesForFeed = [Image]()
-    
+    var imageCache = SDImageCache(namespace: "nameSpaceImageCacheXPTO")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,9 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
 
         })
+        
+        //self.imageCache = SDImageCache(namespace: "nameSpaceImageCacheXPTO")
+        imageCache.maxCacheAge = 60*60*3 //seconds
         
         
     }
@@ -57,6 +60,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         let cell = tableView.dequeueReusableCellWithIdentifier("FeedCell") as? FeedTableViewCell
         let oneImage = imagesForFeed[indexPath.row]
         
+        /**sergio code*/
         if (NSFileManager.defaultManager().fileExistsAtPath(documentPahth()+"image1.jpg")){
             
             let savedImage = UIImage(contentsOfFile: documentPahth()+"image1.jpg")
@@ -73,6 +77,38 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
             })
         }
+         /*end of sergio code */
+        
+        
+//        /**copy from internet**/
+//        imageCache.queryDiskCacheForKey(oneImage.downloadURL, done: {(image: UIImage, cacheType: SDImageCacheType) -> Void in
+//            if image {
+//                completedBlock(image, nil)
+//                // return block
+//            }
+//            else {
+//                /////////
+//                // *    Request Image
+//                /////////
+//                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {() -> Void in
+//                    SDWebImageDownloader.sharedDownloader().downloadImageWithURL(url, options: SDWebImageProgressiveDownload, progress: {(receivedSize: Int, expectedSize: Int) -> Void in
+//                        
+//                        }, completed: {(image: UIImage, data: NSData, error: NSError, finished: Bool) -> Void in
+//                            if finished && image {
+//                                /////////
+//                                // *    Save Image
+//                                /////////
+//                                imageCacheProgram.storeImage(image, recalculateFromImage: false, imageData: data, forKey: key, toDisk: true)
+//                                completedBlock(image, error)
+//                            } //end if
+//                    }) //end completed image:
+//                    
+//                }) //end completed SDWebImageDownloader
+//            } //end else
+//        }) //end queryDiskCacheForKey
+        
+            
+                                
         
 //        cell?.imagePost.sd_setImageWithURL(NSURL(string: oneImage.downloadURL))
         
